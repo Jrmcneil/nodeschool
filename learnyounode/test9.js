1c5ff6cf4e6f9c9c7ab198c3bbcd979b501ff71d
James McNeil <james.r.mcneil@gmail.com>
2014-11-16 11:08:54 +0000

    refactors learnyounode test9 to make it async

--- learnyounode/test9.js
+++ learnyounode/test9.js
@@ -1,63 +1,22 @@
-var bl = require('bl')
 var http = require('http');
-var array =  new Array;
-var arg = process.argv
+var bl = require('bl');
+var array =  [];
+// var urls = process.argv.slice(2);
+var counter = 0;
 
-http.get(arg[2], function callback (response){
-   response.pipe(bl(function(err,data){
-      var data = data.toString();
-      array[0] = data; 
-   }));
-   response.on('end', function(){
-      http.get(arg[3], function callback (response){
-         response.pipe(bl(function(err,data){
-            var data = data.toString();
-            array[1] = data;
-         }));
-         response.on('end', function(){
-            http.get(arg[4], function callback (response){
-               response.pipe(bl(function(err,data){
-                  var data = data.toString();
-                  array[2] = data;
-                  array.forEach(function(value) {console.log(value)})
-               }))
-            })
-         });
-      });
-   });
-})
+function urls(index){
+  http.get(process.argv[2 + index], function callback(response){
+    response.pipe(bl(function(err,data){
+      array[index] = data.toString();
+      counter += 1; 
+      if (array.length === 3) {
+          array.forEach(function(value) {
+            console.log(value)
+        })
+      }
+    }));
+  })
+}
 
-
-// Th nodeschool answer (much simpler):
-    // var http = require('http')
-    // var bl = require('bl')
-    // var results = []
-    // var count = 0
-
-    // function printResults () {
-    //   for (var i = 0; i < 3; i++)
-    //     console.log(results[i])
-    // }
-
-    // function httpGet (index) {
-    //   http.get(process.argv[2 + index], function (response) {
-    //     response.pipe(bl(function (err, data) {
-    //       if (err)
-    //         return console.error(err)
-
-    //       results[index] = data.toString()
-    //       count++
-
-    //       if (count == 3) // yay! we are the last one!
-    //         printResults()
-    //     }))
-    //   })
-    // }
-
-    // for (var i = 0; i < 3; i++)
-    //   httpGet(i)
-
-
-
-
-
+for(var i = 0; i < 3; i++)
+  urls(i)
